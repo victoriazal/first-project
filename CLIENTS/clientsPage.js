c5eaf68bc1c4dc3fca1756168b33de79bd3156ae
DARYA POPOVA <lady.pops@ya.ru>
2022-05-24 01:01:31 +0300

    Update clientsPage.js

--- CLIENTS/clientsPage.js
+++ CLIENTS/clientsPage.js
@@ -1,6 +1,6 @@
 //что-то с фечом
 async function getResponse() {
-    let url = 'https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json';
+    const url = 'https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json';
     let response = await fetch(url);
     let content = await response.json();
 
